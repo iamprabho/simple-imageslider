46c571dcb1fc30080ea5d00c20f53a2f8d4dcbd2
Prathamesh <prathameshbhor000@gmail.com>
2022-01-16 21:47:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let flag = 0;
+
+function controller(x) {
+    flag = flag+x;
+    slideshow(flag);
+
+
+
+}
+  slideshow(flag);
+function slideshow(num) { 
+    let slides = document.getElementsByClassName("slide");
+    if(num > slides.length-1){
+        flag=0;
+        num =0 ;
+
+
+    }
+    else if(num<0){
+        flag=slides.length-1;
+        num =flag;
+
+
+    }
+    for(let  y of slides){
+        y.style.display="none";
+
+
+    }
+    slides[num].style.display="block";
+}
+
